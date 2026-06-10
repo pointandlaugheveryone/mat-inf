01594c0bf6d0dde0344ed11288c316e53468da88
pointandlaugheveryone <ronyi@email.cz>
2026-06-10 17:18:59 +0200

    at least it gets loaded

--- compulsory_homeworks/05_game/my_player.py
+++ compulsory_homeworks/05_game/my_player.py
@@ -5,15 +5,6 @@ from typing import Optional, Tuple
 class StudentBot:
     def __init__(self, name: str = "My Student Bot"):
         self.name = name
-        self.l = ctypes.CDLL("./player.so")
-        # noinspection PyDeprecation
-        self.l.chooseMove.argtypes = [
-            np.ctypeslib.ndpointer(dtype=np.int8, flags="C_CONTIGUOUS"),
-            ctypes.c_int, ctypes.c_int,
-            ctypes.c_int, ctypes.c_int,
-            ctypes.POINTER(ctypes.c_int),ctypes.POINTER(ctypes.c_int)
-        ]
-        self.l.chooseMove.restype = None
 
     def choose_move(
         self,
@@ -21,10 +12,19 @@ class StudentBot:
         player: int,
         win_length: int
     ) -> Optional[Tuple[int, int]]:
+        self.l = ctypes.CDLL("./player.so")
+        # noinspection PyDeprecation
+        self.l.chooseMove.argtypes = [
+            np.ctypeslib.ndpointer(dtype=np.int8, flags="C_CONTIGUOUS"),
+            ctypes.c_int, ctypes.c_int,
+            ctypes.c_int, ctypes.c_int,
+            ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_int)
+        ]
+        self.l.chooseMove.restype = None
 
         r,c = board.shape
         c_board = np.ascontiguousarray(board).astype(np.int8,copy=False)
         outr = ctypes.c_int(-1)
         outc = ctypes.c_int(-1)
-        self.l.ChooseMove(c_board, r,c, player, win_length, ctypes.byref(outr), ctypes.byref(outc))
+        self.l.chooseMove(c_board, r,c, player, win_length, ctypes.byref(outr), ctypes.byref(outc))
         return int(outr.value), int(outc.value)
