f13271356311d577548b3511e05d0662852236d5
pointandlaugheveryone <ronyi@email.cz>
2026-06-10 16:44:36 +0200

    wip

--- compulsory_homeworks/05_game/my_player.py
+++ compulsory_homeworks/05_game/my_player.py
@@ -1,9 +1,19 @@
 import numpy as np
+import ctypes
 from typing import Optional, Tuple
 
 class StudentBot:
     def __init__(self, name: str = "My Student Bot"):
         self.name = name
+        self.l = ctypes.CDLL("./player.so")
+        # noinspection PyDeprecation
+        self.l.chooseMove.argtypes = [
+            np.ctypeslib.ndpointer(dtype=np.int8, flags="C_CONTIGUOUS"),
+            ctypes.c_int, ctypes.c_int,
+            ctypes.c_int, ctypes.c_int,
+            ctypes.POINTER(ctypes.c_int),ctypes.POINTER(ctypes.c_int)
+        ]
+        self.l.chooseMove.restype = None
 
     def choose_move(
         self,
@@ -12,10 +22,9 @@ class StudentBot:
         win_length: int
     ) -> Optional[Tuple[int, int]]:
 
-        empty_cells = np.argwhere(board == 0)
-
-        if len(empty_cells) == 0:
-            return None
-
-        row, col = empty_cells[0]
-        return int(row), int(col)
+        r,c = board.shape
+        c_board = np.ascontiguousarray(board).astype(np.int8,copy=False)
+        outr = ctypes.c_int(-1)
+        outc = ctypes.c_int(-1)
+        self.l.ChooseMove(c_board, r,c, player, win_length, ctypes.byref(outr), ctypes.byref(outc))
+        return int(outr.value), int(outc.value)
--- compulsory_homeworks/05_game/player.cpp
+++ compulsory_homeworks/05_game/player.cpp
@@ -1,66 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef int8_t i8;
-typedef vector<pair<int,int>> vii;
+typedef vector<pair<int, int>> vii;
 
 class Player
 {
-    public:
-        i8* state;
-        int row, col, player, winLength;
+public:
+    i8* state;
+    int row, col;
+    int player, winLength;
 
-    i8 indexAt(const int r,const int c) const
+    i8 indexAt(const int r, const int c) const
     {
         return state[r * col + c];
     }
 
     vii freeCells() const
     {
-        vector<pair<int,int>> empty;
-        for (int r= 0;r<row;r++)
-        {
-            for (int c= 0;c<col;c++)
-            {
-                if (indexAt(r,c) == '0') empty.emplace_back(r,c);
-            }
-        }
+        vector<pair<int, int>> empty;
+        for (int r = 0; r < row; r++)
+            for (int c = 0; c < col; c++)
+                if (indexAt(r, c) == 0) empty.emplace_back(r, c);
+
         return empty;
     }
 
-    bool win(const bool myPlayer) const
+    bool win(const bool myPlayer) const // checks if winLenth is reached in some direction
     {
         constexpr std::pair<int, int> directions[]
         {
-            {0, 1},  {1, 0},  {1, 1}, {1, -1}, // horizon, vertical, downright, downleft
+            {0, 1}, {1, 0}, {1, 1}, {1, -1}, // horizon, vertical, downright, downleft
         };
 
-        for (const auto pair : directions)
+        for (const auto [dx, dy] : directions)
         {
             for (int r = 0; r < row; r++)
             {
                 for (int c = 0; c < col; c++)
                 {
-                    const int endRow = r + (winLength - 1) * pair.first;
-                    const int endCol = c + (winLength - 1) * pair.second;
-
-                    if (endRow < 0 || endRow >= col || endCol < 0 || endCol >= col) continue;
-
+                    const int endrow = r + (winLength - 1) * dx;
+                    const int endcol = c + (winLength - 1) * dy;
+                    if (endrow < 0 || endrow >= row || endcol < 0 || endcol >= col) continue;
+                    bool isWin = true;
                     for (int i = 0; i < winLength; i++)
                     {
-                        if (myPlayer) // max
+                        if (indexAt(r + i * dx, c + i * dy) != player)
                         {
-                            if (indexAt(r + i * pair.first, c + i * pair.second) != player) return false;
-                        }
-                        else // min
-                        {
-                            if (indexAt(r + i * pair.first, c + i * pair.second) == player ||
-                            indexAt(r + i * pair.first, c + i * pair.second) == 0
-                            )
-                                return false;
+                            isWin = false;
+                            break;
                         }
                     }
-
-                    return true;
+                    if (isWin) return true;
                 }
             }
         }
@@ -68,33 +58,78 @@ class Player
     }
 
 
-    int minimaxABprune(bool max)
+    int minimaxABprune(const bool turn, int alpha, int beta, vii &free)
     {
-        if (freeCells().empty())
+        if (win(true)) return 1;
+        if (win(false)) return -1;
+
+        const i8 me = player;
+        const i8 id = (player == 1) ? 2 : 1;
+        if (turn)
         {
-            if (win(true)) return 1;
-            if (win(false)) return -1;
-            return 0;
+            int best = INT_MIN;
+            for (auto [x, y] : free)
+            {
+                state[x * col + y] = me;
+                best = max(best, minimaxABprune(false, alpha, beta, free));
+                state[x * col + y] = 0;
+
+                alpha = max(alpha, best);
+                if (alpha>=beta) break;
+            }
+            return best;
         }
 
-        if (max)
+        int best = INT_MAX;
+        for (auto [x, y] : free)
         {
-            int max = -INT_MIN;
-            for (int i = 0; i < row*col; i++)
-            {
-                if (state[i] == '0') state[i] = '1';
-            }
+            state[x * col + y] = id;
+            best = min(best, minimaxABprune(true, alpha, beta, free));
+            state[x * col + y] = 0;
+
+            beta = min(beta, best);
+            if (alpha>=beta) break;
         }
+        return best;
+    }
+
+    Player(i8* s, const int r,const int c, const int id, const int len)
+    {
+        state = s;
+        row = r;
+        col = c;
+        player = id;
+        winLength = len;
     }
 };
 
 extern "C" {
 
-void chooseMove(int a, int b, bool max)
+void chooseMove(
+    i8* state,
+    const int row, const int col,
+    const int playerID,
+    const int winLength,
+    int* outrow, int* outcol)
 {
+    Player p{state, row, col, playerID, winLength};
+    vii free = p.freeCells();
+    if (free.empty()) return;
 
+    int bestScore = INT_MIN;
+    for (auto [r, c] : free)
+    {
+        state[r * col + c] = playerID;
+        const int score = p.minimaxABprune(false, INT_MIN, INT_MAX, free);
+        state[r * col + c] = 0;
+
+        if (score > bestScore)
+        {
+            bestScore = score;
+            *outrow = r;
+            *outcol = c;
+        }
+    }
 }
-
-
 
 }
